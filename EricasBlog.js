1f42d7e66601f695625b73dd6ad11ea98c13850e
Hatumu <hayden.munck@gmail.com>
2022-01-04 03:22:01 -0800

    .

---
+++ EricasBlog.js
@@ -0,0 +1,12 @@
+function MenuButton(x) {
+    x.classList.toggle("change");
+  } 
+
+  function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+  function myFunction2() {
+    document.getElementById("DropMenu").classList.toggle("show");
+    document.getElementById("Menu").classList.toggle("change");
+  }
